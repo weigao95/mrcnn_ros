cc7f752682ba1e3414a5d864b8add4363ec6054c
wei <gaowei19951004@gmail.com>
2019-05-16 22:31:45 -0400

    update

--- scripts/simple_mrcnn_client_test.py
+++ scripts/simple_mrcnn_client_test.py
@@ -10,8 +10,15 @@ from cv_bridge import CvBridge, CvBridgeError
 def main():
     rospy.wait_for_service('instance_segment')
     instance_segment = rospy.ServiceProxy('instance_segment', InstanceSegmentation)
+
+    # Get the test data path
+    project_path = os.path.join(os.path.dirname(__file__), os.path.pardir)
+    project_path = os.path.abspath(project_path)
+    test_data_path = os.path.join(project_path, 'test_data')
+    cv_rbg_path = os.path.join(test_data_path, '000000_rgb.png')
+
     # Get the data
-    cv_rgb = cv2.imread('/home/wei/data/mankey_pdc_data/mug/0_rgb.png', cv2.IMREAD_COLOR)
+    cv_rgb = cv2.imread(cv_rbg_path, cv2.IMREAD_COLOR)
 
     # Build the request
     request = InstanceSegmentationRequest()
@@ -20,8 +27,8 @@ def main():
 
     # Call the service
     response = instance_segment(request)
-    print response.object_names
-    print response.bounding_boxes
+    print(response.object_names)
+    print(response.bounding_boxes)
 
 
 if __name__ == '__main__':
